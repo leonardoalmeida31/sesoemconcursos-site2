3b50b0d02d9d6471f6da0abbdff02da8ed7eae99
Katianne23 <katianne40@gmail.com>
2023-08-28 15:38:34 -0300

    Firebase insert

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,81 @@
-import React, { useState } from 'react';
-import questoes from '../dados/questoes.json';
-import FiltroMulti from './FiltroMulti';
+import React, { useState, useEffect } from "react";
+import FiltroMulti from "./FiltroMulti";
 import { CaretRight, ChatCenteredText } from "@phosphor-icons/react";
+import "./App.css";
 
-import './App.css'; 
+import { initializeApp } from "firebase/app";
+import { getDocs, getFirestore, collection } from "firebase/firestore";
+
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyBssGj_y0d_q9vOFD3wivAKJkoGMBakEIU",
+  authDomain: "sesoemconcursosweb.firebaseapp.com",
+  projectId: "sesoemconcursosweb",
+};
 
 function App() {
-  const questoesPorPagina = 10;
+  const firebaseApp = initializeApp(firebaseConfig);
+  const db = getFirestore(firebaseApp);
+  const questionsCollectionRef = collection(db, "questions");
+
+  const questoesPorPagina =  10;
+
+  const [questions, setQuestions] = useState([]);
   const [paginaAtual, setPaginaAtual] = useState(1);
-  const [questoesFiltradas, setQuestoesFiltradas] = useState(questoes); // Todas as questões como padrão
+  const [questoesFiltradas, setQuestoesFiltradas] = useState([]);
   const [filtroBanca, setFiltroBanca] = useState(null);
   const [filtroDisciplina, setFiltroDisciplina] = useState(null);
   const [filtroAssunto, setFiltroAssunto] = useState(null);
   const [filtroAno, setFiltroAno] = useState(null);
   const [filtroModalidade, setFiltroModalidade] = useState(null);
   const [filtroArea, setFiltroArea] = useState(null);
-
   const indiceInicial = (paginaAtual - 1) * questoesPorPagina;
-  
- 
 
 
-  const questoesPagina = questoesFiltradas.slice(indiceInicial, indiceInicial + questoesPorPagina);
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+  useEffect(() => {
+    const getQuestions = async () => {
+      const data = await getDocs(questionsCollectionRef);
+      setQuestions(shuffleArray(data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))));
+    };
+    getQuestions();
+  }, []);
+
+  useEffect(() => {
+    const filtered = questions.filter((question) => {
+      return (
+        (!filtroBanca || question.banca === filtroBanca) &&
+        (!filtroDisciplina || question.disciplina === filtroDisciplina) &&
+        (!filtroAno || question.ano === filtroAno) && 
+        (!filtroAssunto || question.assunto === filtroAssunto) &&
+        (!filtroModalidade || question.modalidade === filtroModalidade) 
+      );
+    });
+
+    setQuestoesFiltradas(filtered);
+    setPaginaAtual(1);
+  }, [
+    filtroBanca,
+    filtroDisciplina,
+    filtroAssunto,
+    filtroAno,
+    filtroModalidade,
+    filtroArea,
+    questions,
+  ]);
+
+
+  const questoesPagina = questoesFiltradas.slice(
+    indiceInicial,
+    indiceInicial + questoesPorPagina
+  );
+
   const totalPages = Math.ceil(questoesFiltradas.length / questoesPorPagina);
 
   const handlePreviousPage = () => {
@@ -36,145 +90,157 @@ function App() {
     }
   };
 
-  
-  
 
- 
+  const [alternativasSelecionadas, setAlternativasSelecionadas] = useState({});
 
-
-   const [alternativasSelecionadas, setAlternativasSelecionadas] = useState({});
-
-  const handleAlternativaClick = (questaoId, alternativaIndex) => {
+  const handleAlternativaClick = (questionId, alternativaIndex) => {
     setAlternativasSelecionadas((prevSelecionadas) => ({
       ...prevSelecionadas,
-      [questaoId]: alternativaIndex,
+      [questionId]: alternativaIndex,
     }));
   };
 
   const [feedback, setFeedback] = useState({});
-  const verificarResposta = (questao) => {
-    const alternativaSelecionada = alternativasSelecionadas[questao.id];
-    const respostaCorreta = questao.resposta.charCodeAt(0) - 65;
-  
+  const verificarResposta = (question) => {
+    const alternativaSelecionada = alternativasSelecionadas[question.id];
+    const respostaCorreta = question.resposta.charCodeAt(0) - 65;
+
     if (alternativaSelecionada === respostaCorreta) {
       setFeedback((prevFeedback) => ({
         ...prevFeedback,
-        [questao.id]: 'Você acertou!',
+        [question.id]: "Você acertou!",
       }));
     } else {
       setFeedback((prevFeedback) => ({
         ...prevFeedback,
-        [questao.id]: 'Você errou!',
+        [question.id]: "Você errou!",
       }));
     }
   };
 
-
   const [comentariosVisiveis, setComentariosVisiveis] = useState({});
 
-  const toggleComentario = (questaoId) => {
-    console.log("Toggled comentario for questaoId:", questaoId);
+  const toggleComentario = (questionId) => {
+    console.log("Toggled comentario for questionId:", questionId);
     setComentariosVisiveis((prevVisiveis) => ({
       ...prevVisiveis,
-      [questaoId]: !prevVisiveis[questaoId],
+      [questionId]: !prevVisiveis[questionId],
     }));
   };
 
-
-
-  const handleFilterChange = (filteredQuestoes) => {
-    setQuestoesFiltradas(filteredQuestoes);
-    setPaginaAtual(1);
-  };
-  
-  
-  
   return (
-    
     <div className="App">
-<div className="campo-nome-home"><h2 className="nome-home">SESO em Concursos</h2></div>
-    
-<FiltroMulti onFilterChange={handleFilterChange} />
-
-      
-
-{questoesPagina.map((questao) => (
-  <div key={questao.id} className="question-container">
-    <div className="cabecalho-disciplina">
-     
-    <p>ID: {questao.id}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{questao.disciplina}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{questao.assunto}</p>
-
-      
-    </div>
-    <div className="cabecalho-orgao">
-    <p>Banca: {questao.banca}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ano: {questao.ano}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Cargo: {questao.cargo}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
-    <p>Órgão: {questao.concurso}</p>
-    </div>
-    <p className="enunciado">{questao.enunciado}</p>
-    <ul>
-  {questao.alternativas.map((alternativa, index) => {
-    const letraAlternativa = alternativa.match(/^\(([A-E])\)/)[1];
-
-
-    
-
-    return (
-      <li
-        className={`alternativa ${alternativasSelecionadas[questao.id] === index ? 'selecionada' : ''}`}
-        key={index}
-        onClick={() => handleAlternativaClick(questao.id, index)}
-      >
-        <span
-          className={`letra-alternativa-circle ${alternativasSelecionadas[questao.id] === index ? 'selecionada' : ''}`}
-        >
-          {letraAlternativa}
-        </span>
-        {alternativa.replace(/^\(([A-E])\)/, '')}
-      </li>
-    );
-  })}
-</ul>
-
-<div className="button-feedback-container">
-<button className="button-responder" onClick={() => verificarResposta(questao)}>
-  Responder
-</button>
-
-
-{feedback[questao.id] && (
-  <p className={`feedback ${feedback[questao.id] === 'Você acertou!' ? 'acerto' : 'erro'}`}>
-    {feedback[questao.id] === 'Você acertou!' ? 'Você acertou!' : `Você errou! A alternativa correta é: ${questao.resposta}`}
-  </p>
-)}
-</div>
-
-  
-
-<div className="linha-horizontal-comentario"></div>
-
-  <div className="campo-comentario">  
-<button className="button-comentario" onClick={() => toggleComentario(questao.id)}> <ChatCenteredText size={14}  />  Comentário do Professor
-      </button>
-
-      <p className={comentariosVisiveis[questao.id] ? "comentario visivel" : "comentario"}>
-        {questao.comentario}
-      </p>
+      <div className="campo-nome-home">
+        <h2 className="nome-home">SESO em Concursos</h2>
       </div>
-    </div>
-))}
- 
+
+      <FiltroMulti onFilterChange={setQuestoesFiltradas}  />
+      
+
+      {questoesPagina.map((question, index) => (
+        <div key={question.id} className="question-container">
+          <div className="cabecalho-disciplina">
+            <p>
+              ID: {question.ids}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {question.disciplina}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {question.assunto}
+            </p>
+          </div>
+          <div className="cabecalho-orgao">
+            <p>
+              Banca: {question.banca}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ano:{" "}
+              {question.ano}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Cargo:{" "}
+              {question.cargo}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </p>
+            <p>Órgão: {questions.concurso}</p>
+          </div>
+          <p className="enunciado">{question.enunciado}</p>
+          <ul>
+            {question.alternativas.map((alternativa, index) => {
+              const letraAlternativa = alternativa.match(/^\(([A-E])\)/)[1];
+
+              return (
+                <li
+                  className={`alternativa ${
+                    alternativasSelecionadas[question.id] === index
+                      ? "selecionada"
+                      : ""
+                  }`}
+                  key={index}
+                  onClick={() => handleAlternativaClick(question.id, index)}
+                >
+                  <span
+                    className={`letra-alternativa-circle ${
+                      alternativasSelecionadas[question.id] === index
+                        ? "selecionada"
+                        : ""
+                    }`}
+                  >
+                    {letraAlternativa}
+                  </span>
+                  {alternativa.replace(/^\(([A-E])\)/, "")}
+                </li>
+              );
+            })}
+          </ul>
+          <div className="button-feedback-container">
+            <button
+              className="button-responder"
+              onClick={() => verificarResposta(question)}
+            >
+              Responder
+            </button>
+
+            {feedback[question.id] && (
+              <p
+                className={`feedback ${
+                  feedback[question.id] === "Você acertou!" ? "acerto" : "erro"
+                }`}
+              >
+                {feedback[question.id] === "Você acertou!"
+                  ? "Você acertou!"
+                  : `Você errou! A alternativa correta é: ${question.resposta}`}
+              </p>
+            )}
+          </div>
+
+          <div className="linha-horizontal-comentario"></div>
+
+          <div className="campo-comentario">
+            <button
+              className="button-comentario"
+              onClick={() => toggleComentario(question.id)}
+            >
+              {" "}
+              <ChatCenteredText size={14} /> Comentário do Professor
+            </button>
+
+            <p
+              className={
+                comentariosVisiveis[question.id]
+                  ? "comentario visivel"
+                  : "comentario"
+              }
+            >
+              {question.comentario}
+            </p>
+          </div>
+        </div>
+      ))}
 
       <div className="pagination">
         <button onClick={handlePreviousPage} disabled={paginaAtual === 1}>
           Página Anterior
         </button>
-        <span>Página {paginaAtual} de {totalPages}</span>
+        <span>
+          Página {paginaAtual} de {totalPages}
+        </span>
         <button onClick={handleNextPage} disabled={paginaAtual === totalPages}>
           Próxima Página
         </button>
       </div>
     </div>
   );
-}
+            }
 
 export default App;
