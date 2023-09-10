2c0526f03eef8e9c3e19ffd503ec5ffb86a38163
leonardoalmeida31 <leonardoalmeida10515@gmail.com>
2023-09-10 14:55:10 -0300

    page perfil com grafico 1

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/home.jsx'
-import PlanoEstudos from './pages/Plano-Estudos.jsx'
+import MeuPerfil from './pages/MeuPerfil.jsx'
 
 
 
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Plano-Estudos" element={<PlanoEstudos />} />
+        <Route path="/MeuPerfil" element={<MeuPerfil />} />
 
       </Routes>
     </React.StrictMode>
